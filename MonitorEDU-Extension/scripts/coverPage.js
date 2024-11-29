6d19667acec7aa2eb2bdd1529e343b400f835362
Jose Arias <josearias@monitoredu.com>
2024-11-28 23:35:37 -0400

    Update coverPage.js
    Click event to check for tabs and alert if greater than 3.

--- MonitorEDU-Extension/scripts/coverPage.js
+++ MonitorEDU-Extension/scripts/coverPage.js
@@ -1,3 +1,17 @@
+// Tabs counter
+function checktabs(){
+  var port = chrome.runtime.connect({name: "tabCounter"});
+  const maxTabs = 3;
+
+  port.postMessage({ask: "How Many Tabs?"});
+
+  port.onMessage.addListener(function(msg) {
+    if (msg.response > maxTabs) {
+      alert("Too many tabs open");
+    }  
+  });
+}
+
 function resetView() {
   if (document.hasFocus()) {
     const overlay = document.getElementById("overlay-monitoredu");
@@ -49,3 +63,5 @@ window.addEventListener("focus", () => {
 });
 
 setInterval(resetClipBoard, 5000);
+
+window.addEventListener("click", checktabs);
