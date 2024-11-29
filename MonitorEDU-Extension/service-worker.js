3d44f9cdb15a1544113c8fcad8ff1fc4bd85ed88
Jose Arias <josearias@monitoredu.com>
2024-11-28 23:33:44 -0400

    Update service-worker.js
    A listener to return the current tab count.

--- MonitorEDU-Extension/service-worker.js
+++ MonitorEDU-Extension/service-worker.js
@@ -1,10 +1,10 @@
-chrome.action.onClicked.addListener(buttonClicked);
-
-function buttonClicked(tab){
-  chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-    let openTabs = {
-      tabCount: tabs.length
+chrome.runtime.onConnect.addListener(function(port) {
+  console.assert(port.name === "tabCounter");
+  port.onMessage.addListener(function(msg) {
+    if (msg.ask === "How Many Tabs?") {
+      chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+        port.postMessage({response: tabs.length});
+      });
     }
-    console.log(openTabs);
   });
-}
+});
