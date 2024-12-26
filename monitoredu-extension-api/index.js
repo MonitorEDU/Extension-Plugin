a2451491f97e89b60336cf7b69bba3cf0ac88658
samgates3 <102706310+sgates464@users.noreply.github.com>
2024-12-26 09:37:55 -0500

    add headers for CORS

--- monitoredu-extension-api/index.js
+++ monitoredu-extension-api/index.js
@@ -4,10 +4,17 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
+// ensure responses have headers that allow CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.route("/events").post((req, res) => {
   console.log(req.body);
-  res.status(200).send("Event received");
+  res.status(200).json({ message: "Event received" });
 });
 
 app.route("/init").get((req, res) => {
