1cbce02535d2a1f6d1c1fb188e7d3d8731e0d7f5
samgates3 <102706310+sgates464@users.noreply.github.com>
2025-03-14 09:41:56 -0400

    Create index.js

---
+++ monitoredu-extension-api/index.js
@@ -0,0 +1,159 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
+const app = express();
+const bodyParser = require("body-parser");
+const { default: axios } = require("axios");
+const endpoint = process.env.GQL_ENDPOINT;
+const key = process.env.GQL_API_KEY;
+
+const sqs = new SQSClient({ region: process.env.AWS_REGION });
+const queueUrl = process.env.SQS_QUEUE_URL;
+
+app.use(bodyParser.json());
+// ensure responses have headers that allow CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  console.log(req);
+  next();
+});
+
+app.route("/events").post(async (req, res) => {
+  console.log(req.body);
+  const { session_code, event, event_description, event_timestamp } = req.body;
+  try {
+    const params = {
+      MessageBody: JSON.stringify({
+        session_code,
+        event,
+        event_description,
+        event_timestamp,
+      }),
+      QueueUrl: queueUrl,
+      MessageGroupId: session_code, // For FIFO queues
+      MessageDeduplicationId: `${session_code}-${Date.now()}`, // Unique ID for each message
+    };
+
+    await sqs.send(new SendMessageCommand(params));
+    res.status(200).json({ message: "Event queued successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.route("/init").get((req, res) => {
+  if (!req.query.session_code) {
+    return res.status(400).json({ error: "session_code is required" });
+  }
+
+  const session_code = req.query.session_code;
+
+  GQLPoster(GetSessionQuery, { session_code })
+    .then((response) => {
+      console.log(response);
+      if (response.data.getExtensionSession === null) {
+        return res.status(404).json({
+          error: "Session not found for session_code: " + session_code,
+        });
+      } else {
+        res.status(200).json(response.data.getExtensionSession);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+});
+
+app.route("/status").get((req, res) => {
+  if (!req.query.session_code) {
+    return res.status(400).json({ error: "session_code is required" });
+  }
+  const session_code = req.query.session_code;
+
+  GQLPoster(GetSessionQuery, { session_code })
+    .then((response) => {
+      console.log(response);
+      if (response.data.getExtensionSession === null) {
+        return res.status(404).json({
+          error: "Session not found for session_code: " + session_code,
+        });
+      } else {
+        res.status(200).json(response.data.getExtensionSession);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+});
+
+module.exports.handler = serverless(app);
+
+async function GQLPoster(query, variables) {
+  const headers = {
+    "x-api-key": key,
+  };
+
+  let funcResponse;
+
+  await axios
+    .post(endpoint, JSON.stringify({ query, variables }), { headers })
+    .then((res) => {
+      console.log(res.data);
+      funcResponse = res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw new Error(err);
+    });
+
+  return funcResponse;
+}
+
+const UpdateEventQuery = `
+  mutation UpdateExtensionSession($input: UpdateExtensionSessionInput!) {
+    updateExtensionSession(input: $input) {
+      session_code
+      events {
+        event
+        event_description
+        event_timestamp
+        }
+      status
+      type
+    }
+  }
+`;
+
+const GetSessionQuery = `
+  query GetExtensionSession($session_code: ID!) {
+    getExtensionSession(session_code: $session_code) {
+      session_code
+      status
+      type
+      exam_url
+      max_tabs
+      keep_alive_interval
+      maximum_duration
+    }
+  }
+`;
+
+const FetchEventsToUpdateQuery = `
+  query GetExtensionSession($session_code: ID!) {
+    getExtensionSession(session_code: $session_code) {
+      session_code
+      status
+      type
+      events {
+        event
+        event_description
+        event_timestamp
+      }
+    }
+  }
+`;
